8ae019e4a314cebd09955a81ed341e3254a03375
mohammed-alhanjouri <mohammadalhanjouri@gmail.com>
2026-07-08 11:32:05 +0300

    Add Playwright configuration to ESLint for test files

--- eslint.config.js
+++ eslint.config.js
@@ -19,4 +19,13 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
+  {
+    files: ['playwright.config.ts', 'tests/**/*.ts'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
 ])
